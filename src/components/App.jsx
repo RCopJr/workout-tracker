9d7631e0c7e11d0f653be6f9af40ef78f51930d6
Ramonito Copreros Jr <rcoprerosjr@gmail.com>
2022-11-30 22:38:49 -0500

    Update App and RoutinePreviewCard components to work with Workouts component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,12 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import "../App.css";
 import BtnSolid from "./BtnSolid";
 import ExerciseHeading from "./ExerciseHeading";
 import RoutinePreviewCard from "./RoutinePreviewCard";
+import Workouts from "./Workouts";
 
 function App() {
-  const [workout, setWorkout] = useState([
-    {
-      workoutName: "",
-      exercises: [
-        {
-          exerciseName: "",
-          sets: [
-            {
-              set: 0,
-              weight: 0,
-              reps: "",
-              rest: 0,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
-
-  useEffect(() => {
-    const getWorkout = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/");
-        setWorkout(response);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getWorkout();
-  }, []);
-
-  return (
-    <div className="App">
-      <RoutinePreviewCard />
-    </div>
-  );
+  const { workoutId, setWorkoutId } = useState({});
+  return <Workouts workoutId={workoutId} setWorkoutId={setWorkoutId} />;
 }
 
 export default App;
--- src/components/RoutinePreviewCard.jsx
+++ src/components/RoutinePreviewCard.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import { useQuery } from "react-query";
+import * as api from "../workoutsAPI";
 import RoutineModal from "./RoutineModal";
 
 const RoutinePreviewCard = (props) => {
+  const { data, isLoading } = useQuery(["workouts", props.workoutId], () =>
+    api.getWorkout(props.workoutId)
+  );
   const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
@@ -12,6 +17,24 @@ const RoutinePreviewCard = (props) => {
     setIsOpen(false);
   };
 
+  if (isLoading) {
+    return (
+      <>
+        <div
+          onClick={() => {
+            openModal();
+          }}
+          className="block m-10 max-w-xs p-3 bg-white border border-gray-100 rounded-lg hover:bg-gray-100"
+        >
+          <h1 className="font-bold font-sans pb-2">Loading...</h1>
+          <p className="text-slate-600"></p>
+        </div>
+        <RoutineModal closeModal={closeModal} isOpen={isOpen} />
+      </>
+    );
+  }
+  const lenExercises = data.workout.exercises.length;
+
   return (
     <>
       <div
@@ -20,8 +43,19 @@ const RoutinePreviewCard = (props) => {
         }}
         className="block m-10 max-w-xs p-3 bg-white border border-gray-100 rounded-lg hover:bg-gray-100"
       >
-        <h1 className="font-bold font-sans pb-2">Legs</h1>
-        <p className="text-slate-600">Squat and Leg Extension</p>
+        <h1 className="font-bold font-sans pb-2">{data.workout.name}</h1>
+        <p className="text-slate-600">
+          {data.workout.exercises.reduce((previewInfo, currExercise, index) => {
+            let separator = ", ";
+            if (index === lenExercises - 1) {
+              separator = "";
+            }
+            return (
+              previewInfo +
+              `${currExercise.sets.length} x ${currExercise.name}${separator}`
+            );
+          }, "")}
+        </p>
       </div>
       <RoutineModal closeModal={closeModal} isOpen={isOpen} />
     </>
