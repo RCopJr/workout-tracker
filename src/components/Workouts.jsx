1f26906e36d3a25bc517becbc91a3e7f9df429db
Ramonito Copreros Jr <rcoprerosjr@gmail.com>
2022-12-03 00:19:21 -0500

    Fix window focus error

--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useQuery } from "react-query";
+import { v4 as uuidv4 } from "uuid";
 import * as api from "../workoutsAPI";
 import WorkoutPreviewCard from "./WorkoutPreviewCard";
 
 const Workouts = (props) => {
-  const { data, isLoading, isError } = useQuery("workouts", api.getWorkouts);
+  const { data, isLoading, isError } = useQuery("workouts", api.getWorkouts, {
+    refetchOnWindowFocus: false,
+  });
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -13,11 +16,11 @@ const Workouts = (props) => {
   if (isError) {
     return <p>Error Getting Data</p>;
   }
-
   return (
     <>
       {data.workouts.map((workout) => {
-        return <WorkoutPreviewCard key={workout.id} workoutId={workout.id} />;
+        const id = uuidv4();
+        return <WorkoutPreviewCard key={id} workoutId={workout.id} />;
       })}
     </>
   );
