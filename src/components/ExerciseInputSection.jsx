83b27784c3b5c54603352575db127a4fdef06775
Ramonito Copreros Jr <rcoprerosjr@gmail.com>
2022-12-04 18:38:33 -0500

    Added animations to sets

--- src/components/ExerciseInputSection.jsx
+++ src/components/ExerciseInputSection.jsx
@@ -1,10 +1,12 @@
-import React, { Fragment, useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import React from "react";
 import BtnSolid from "./BtnSolid";
 import ExerciseDataHeader from "./ExerciseDataHeader";
 import ExerciseHeadingInput from "./ExerciseHeadingInput";
 import SetEntryInput from "./SetEntryInput";
 
 const ExerciseInputSection = (props) => {
+  const [animationParent] = useAutoAnimate({ duration: 100 });
   return (
     <div>
       <ExerciseHeadingInput
@@ -15,23 +17,25 @@ const ExerciseInputSection = (props) => {
         handleChangeExercise={props.handleChangeExercise}
       />
       <ExerciseDataHeader inEditMode={props.inEditMode} />
-      {props.setIds.map((setId, index) => {
-        const { weight, reps, rest } = props.fields.sets[setId];
-        return (
-          <SetEntryInput
-            key={setId}
-            exerciseId={props.exerciseId}
-            originalSets={props.workout.sets}
-            setId={setId}
-            setNum={index + 1}
-            weight={weight}
-            reps={reps}
-            rest={rest}
-            handleRemoveSet={props.handleRemoveSet}
-            handleChangeSet={props.handleChangeSet}
-          />
-        );
-      })}
+      <div ref={animationParent}>
+        {props.setIds.map((setId, index) => {
+          const { weight, reps, rest } = props.fields.sets[setId];
+          return (
+            <SetEntryInput
+              key={setId}
+              exerciseId={props.exerciseId}
+              originalSets={props.workout.sets}
+              setId={setId}
+              setNum={index + 1}
+              weight={weight}
+              reps={reps}
+              rest={rest}
+              handleRemoveSet={props.handleRemoveSet}
+              handleChangeSet={props.handleChangeSet}
+            />
+          );
+        })}
+      </div>
       <div className="mt-5 flex justify-center gap-3">
         <BtnSolid
           variant="gray-sm"
